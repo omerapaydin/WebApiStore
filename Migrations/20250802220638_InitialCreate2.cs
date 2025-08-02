89efb98762b446c249435db8bb98f4a22756a88f
Ömer Apaydın <omerapaydin90@gmail.com>
2025-08-03 01:08:07 +0300

    HasData

---
+++ Migrations/20250802220638_InitialCreate2.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsActive", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "Apple Watch Series", "1.jpg", true, "Apple Watch Series 1", 70000m, 100 },
+                    { 2, "Telefon açıklaması", "2.jpg", true, "Apple Watch Series 2", 80000m, 100 },
+                    { 3, "Telefon açıklaması", "3.jpg", false, "Apple Watch Series 3", 90000m, 100 },
+                    { 4, "Telefon açıklaması", "4.jpg", true, "Xiaomi Redmi Watch 1", 100000m, 100 },
+                    { 5, "Telefon açıklaması", "5.jpg", true, "Xiaomi Redmi Watch 2", 100000m, 100 },
+                    { 6, "Telefon açıklaması", "6.jpg", true, "Xiaomi Redmi Watch 3", 100000m, 100 },
+                    { 7, "Telefon açıklaması", "7.jpg", true, "Xiaomi Redmi Watch 4", 100000m, 100 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
